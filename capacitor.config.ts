23943daa0b9ae8714816f02b2acbf9cf11fe7ada
caioST <dasilvacaio919@gmail.com>
2024-09-17 21:01:53 -0300

    alteração appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tech.finchurchApp',
   appName: 'ionic-finchurch-app',
   webDir: 'www',
     plugins: {
